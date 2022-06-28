1f9ca4a2e72f346e17d1f9804d9fff7956e8b98e
xiaoxunyao <huchengyea@163.com>
2022-06-28 13:28:02 +0800

    fix: update core

--- test/src/main.ts
+++ test/src/main.ts
@@ -2,7 +2,4 @@ import { createApp } from 'vue'
 import App from '~/App.vue'
 import router from '~/router'
 
-import '@unocss/reset/tailwind.css'
-import 'uno.css'
-
 createApp(App).use(router).mount('#app')
--- test/src/pages/index.vue
+++ test/src/pages/index.vue
@@ -1,104 +1,35 @@
 <script setup lang="ts">
 import keyboard from '@elonehoo/keyboard'
+import macKeyboard, { KeyCodeData } from '@elonehoo/vue-mac-keyboard'
 
-// custom activation function
-const activate = (event: any, handler: any) => {
-  const { key } = handler
-  const elmt = document.querySelector(`.button-${key}`)
-  if (elmt) {
-    elmt.classList.add('active')
-    setTimeout(() => { elmt.classList.remove('active') }, 350)
-  }
+const keycode: number[] = $ref([])
+
+document.addEventListener('keyup',onKeyUpEvent)
+
+function onKeyUpEvent(){
+  keycode.splice(0,keycode.length)
 }
 
-keyboard('a,b,c,7', activate)
+keyboard('*',(event:any)=>{
+  event.preventDefault()
+  keycode.push(event.keyCode)
+})
+
+function macMousedown(el: HTMLLIElement, item: KeyCodeData) {
+  if (item.keycode > -1)
+    keycode.push(item.keycode)
+}
+
+function macMouseup(el: HTMLLIElement, item: KeyCodeData) {
+  keycode.splice(0, keycode.length)
+}
 </script>
 
 <template>
-  <div class="body">
-    <p>PRESS ONE OF US ON YOUR KEYBOARD 😙 <br> <small>(FOCUS THIS WINDOW FIRST)</small></p>
-
-    <div class="keys">
-      <button class="button-a">
-        A
-      </button>
-      <button class="button-b">
-        B
-      </button>
-      <button class="button-c">
-        C
-      </button>
-      <button class="button-7">
-        7
-      </button>
-    </div>
+  <div>
+    <macKeyboard :key-code="keycode" @mac-mousedown="macMousedown" @mac-mouseup="macMouseup" />
   </div>
 </template>
 
 <style scoped>
-.body {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 0;
-  width: 100vw;
-  height: 100vh;
-  background: mediumslateblue;
-}
-
-.keys {
-  margin: 0;
-  display: flex;
-  background: slateblue;
-  padding: 2rem;
-  border-radius: .66rem;
-}
-
-p {
-  text-align: center;
-  margin: 0 0 1rem 0;
-  color: white;
-  font-weight: bold;
-  text-transform: uppercase;
-  font-family: sans-serif;
-}
-
-a {
-  text-align: center;
-  margin: 1rem 0 0 0;
-  color: white !important;
-  font-weight: bold;
-  text-transform: uppercase;
-  font-family: sans-serif;
-  font-size: .7em;
-  text-decoration: none;
-}
-
-.keys button {
-  appearance: none;
-  -webkit-appearance: none;
-  cursor: pointer;
-  border: none;
-  outline: none;
-  background: white;
-  border-radius: .33rem;
-  font-size: 1em;
-  padding: 1rem;
-  margin-right: 1rem;
-  min-width: 5rem;
-  box-shadow: 0 .33rem 1rem rgba(0,0,0,.25);
-  transition: all .15s linear;
-  font-weight: bold;
-  color: slateblue;
-}
-
-.keys button:last-child {
-  margin-right: 0;
-}
-
-.keys button.active {
-  opacity: .6;
-  box-shadow: 0 0 0 rgba(0,0,0,0);
-}
 </style>
